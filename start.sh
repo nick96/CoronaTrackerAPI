9f89c264ba749b79d31f2f5eed664a5386bc7e34
Nick Spain <nicholas.spain96@gmail.com>
2020-04-10 15:16:06 +1000

    Fix up start up script to be inline with heroku

--- start.sh
+++ start.sh
@@ -3,6 +3,6 @@
 echo "==> Updating database to latest"
 alembic upgrade head
 
-app_port="${POST:-8000}"
+app_port="${PORT:-8000}"
 echo "==> Starting gunicorn on port ${app_port}"
 gunicorn app:app --bind 0.0.0.0:$app_port --workers 4 --log-file -
